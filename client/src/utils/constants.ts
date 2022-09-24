f52b0d63d40481d5c3fdb3b4747e9cdffeac2472
alvinscheibe <alvin.scheibe@gmail.com>
2022-09-24 01:00:17 -0300

    Config hardhat and alchemy. Adding dotenv

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractAbi = abi.abi;
+export const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -17,4 +17,4 @@ const runMain = async () => {
   }
 }
 
-await runMain();
+runMain();
